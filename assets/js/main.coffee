1f26b66912bec0fe718f39485a7168122a51e05b
Eli Williamson <eliwilliamson@gmail.com>
2015-11-16 23:35:49 -0600

    deploy from ship

--- assets/js/main.coffee
+++
@@ -1,9 +0,0 @@
-$(document).ready ->
-  screen = $(window)
-  aboutCarousel = $(".aboutHero #myCarousel")
-  productIndicators = $(".productsHero .carousel-indicators")
-  if screen.width() < 600
-    aboutCarousel.removeAttr "id"
-    aboutCarousel.removeAttr "class"
-    productIndicators.removeAttr "class"
-    aboutCarousel.removeAttr "data-ride"
---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function() {
+  $(document).ready(function() {
+    var aboutCarousel, productIndicators, screen;
+    screen = $(window);
+    aboutCarousel = $(".aboutHero #myCarousel");
+    productIndicators = $(".productsHero .carousel-indicators");
+    if (screen.width() < 600) {
+      aboutCarousel.removeAttr("id");
+      aboutCarousel.removeAttr("class");
+      productIndicators.removeAttr("class");
+      return aboutCarousel.removeAttr("data-ride");
+    }
+  });
+
+}).call(this);
+
+ //# sourceMappingURL=main.js.map
